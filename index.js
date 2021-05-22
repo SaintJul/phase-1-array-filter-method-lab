024cfdc5d8b34f793d2efa72eeb854a491639f18
SaintJul <favor.julius@gmail.com>
2021-05-22 06:35:22 -0400

    commit tree cbf6af36deb2bb5b2ba9dd91bdc83c3db5f278cf

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution here
+function findMatching(arry, name){
+    const newarry = []
+   for (const drivers of arry){
+       if (drivers === name || drivers.toUpperCase() === name.toUpperCase()){
+           newarry.push(drivers);
+       }      
+   } 
+ return newarry
+}
+
+function fuzzyMatch(arry, str){
+    const newarry = []   
+    for (const drivers of arry){
+        if (drivers.substring(0,2) === str){
+            newarry.push(drivers)
+
+        }
+    }
+    return newarry
+}
+function matchName(arry, str){
+    const newarry = []
+    for(const drivers of arry){
+        if (drivers.name === str){
+            newarry.push(drivers);
+        }
+    }
+    return newarry
+
+}
+
+
+
